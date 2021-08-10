4affa129304851e16f5efdb41e6852dc32fdb021
RaulORios <raul.orios89@outlook.com>
2021-08-10 11:23:19 -0300

    Arquivos atualizados

---
+++ Uri_1008/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, horas;
+            double salario_Hora, salario;
+
+            numero = int.Parse(Console.ReadLine());
+            horas = int.Parse(Console.ReadLine());
+            salario_Hora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = horas * salario_Hora;
+
+            Console.WriteLine($"NUMBER = {numero}");
+            Console.WriteLine($"SALARY = U$ {salario.ToString("F2",CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
